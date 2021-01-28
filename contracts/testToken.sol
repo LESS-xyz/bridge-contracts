f8566eac9420fbecc4904009b99bfe1232a4e982
TepNik <tepelin.nikita@gmail.com>
2021-01-28 17:24:48 +0300

    Chaged testToken

--- contracts/testToken.sol
+++ contracts/testToken.sol
@@ -3,18 +3,33 @@
 pragma solidity ^0.7.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
-contract testToken is ERC20
+contract testToken is ERC20, Ownable
 {
+    using SafeMath for uint256;
+
+    uint256 public maxTotalSupply;
+
     constructor(
         string memory name,
         string memory symbol,
-        uint256 totalSupply,
+        uint256 _maxTotalSupply,
         uint8 decimals
     )
         ERC20(name, symbol)
     {
         _setupDecimals(decimals);
-        _mint(msg.sender, totalSupply);
+        maxTotalSupply = _maxTotalSupply;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner
+    {
+        require(
+            totalSupply().add(amount) < maxTotalSupply,
+            "Token: Total supply will exceed max total supply"
+        );
+        _mint(to, amount);
     }
 }
